7fe8d5af3dfbc4da4275dcd8f4b728f3e99bc563
Clark Kampfe <clark@bellycard.com>
2015-09-13 13:15:17 -0500

    remove theme, renderer

--- lib/obelisk/renderer.ex
+++
@@ -1,4 +0,0 @@
-defmodule Obelisk.Renderer do
-  def render(content, format), do: render(content, [], format)
-  def render(content, params, :eex), do: EEx.eval_string(content, assigns: params)
-end
