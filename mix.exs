886b5a14acb7fb8202cb22dddc275d702f5a899a
Clark Kampfe <clark@bellycard.com>
2015-10-07 23:58:43 -0500

    github link

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Obelisk.Mixfile do
     %{
       licenses: ["MIT"],
       contributors: ["Benny Hallett", "Clark Kampfe"],
-      links: %{"Github" => "https://github.com/bennyhallett/obelisk"}
+      links: %{"Github" => "https://github.com/ckampfe/obelisk"}
      }
   end
 end
