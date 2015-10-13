85a6087087fe8dd323161a02b54010e86d746bc8
Clark Kampfe <clark@bellycard.com>
2015-10-12 23:44:39 -0500

    update post exception grammar and comments

--- lib/mix/tasks/page.ex
+++ lib/mix/tasks/page.ex
@@ -2,7 +2,7 @@ defmodule Mix.Tasks.Obelisk.Page do
   use Mix.Task
 
   @moduledoc """
-  This task creates a new page with the given post title argument as part of the filename,
+  This task creates a new page with the given page title argument as part of the filename,
   and included in the front matter title
 
   ## Arguments
@@ -11,7 +11,7 @@ defmodule Mix.Tasks.Obelisk.Page do
 
   ## Usage
 
-      $ mix obelisk page "This one wierd trick"
+      $ mix obelisk page "This one weird trick"
   """
 
   @shortdoc "Create a new page"
@@ -19,6 +19,6 @@ defmodule Mix.Tasks.Obelisk.Page do
   @doc """
   Run the build task
   """
-  def run([]), do: raise(ArgumentError, message: "Cannot create a new page without the post name")
+  def run([]), do: raise(ArgumentError, message: "Cannot create a new page without the page name")
   def run(args), do: hd(args) |> Obelisk.Page.create
 end
--- test/page_command_test.exs
+++ test/page_command_test.exs
@@ -16,7 +16,7 @@ defmodule PageTaskTest do
 
   test "Command should fail if no args are passed" do
     Mix.Tasks.Obelisk.Init.run([])
-    assert_raise ArgumentError, "Cannot create a new page without the post name", fn ->
+    assert_raise ArgumentError, "Cannot create a new page without the page name", fn ->
       Mix.Tasks.Obelisk.Page.run([])
     end
   end
